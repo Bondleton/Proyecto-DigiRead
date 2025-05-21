820fea34c4c8ba5539bd9c390c5f7534d6a05bde
Bondleton <itzelf182@gmail.com>
2025-05-21 14:42:13 -0600

    Buscar

---
+++ DigiRead/js/home.js
@@ -0,0 +1,19 @@
+// TODO: Este aun no sirve nada   
+
+// // Función para cerrar sesión
+// function logout() {
+//   firebase.auth().signOut().then(() => {
+//     window.location.href = "Index.html";
+//   }).catch((error) => {
+//     console.error("Error al cerrar sesión:", error);
+//   });
+// }
+
+// // Verificar autenticación
+// firebase.auth().onAuthStateChanged((user) => {
+//   if (!user) {
+//     window.location.href = "index.html";
+//   }
+// });
+
+
